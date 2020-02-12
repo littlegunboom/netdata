345d2cfb127f9aaade89674f73c7cd498490295a
Dylan Wang <wanghaoyu@frazil.me>
2020-02-12 14:27:06 +0800

    allow space in tag value

--- backends/prometheus/backend_prometheus.c
+++ backends/prometheus/backend_prometheus.c
@@ -585,7 +585,7 @@ inline static void remote_write_split_words(char *str, char **words, int max_wor
         if(*s)
             words[i] = s;
 
-        while(*s && !isspace(*s) && *s != ',') s++; // find the end of the tag value
+        while(*s && *s != ',') s++; // find the end of the tag value
 
         if(*s && *s != ',') {
             words[i] = NULL;
